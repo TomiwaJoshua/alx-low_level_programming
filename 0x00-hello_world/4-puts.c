def9620d8e95ea66af5148a6f0da70623a72c5b8
Tomiwa Awogun <jawogun@gmail.com>
2022-09-08 00:20:29 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 int main(void)
-{
-char words[] = "\"Programming is like building a multilingual puzzle";
+{char words[] = "\"Programming is like building a multilingual puzzle";
 puts(words);	
-return (0);
-}
+return (0);}
