b5f9946439820769ee1d7553ac3ac4738a728286
Tomiwa Awogun <jawogun@gmail.com>
2022-09-08 02:27:59 -0700

    changes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char character;
+	int integer;
+	long longx;
+	long long longlongx;
+	float floatx;
+	printf("Size of a char: %lu byte(s)\n", sizeof(character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longx));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongx));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatx));
+	return (0);
+}
