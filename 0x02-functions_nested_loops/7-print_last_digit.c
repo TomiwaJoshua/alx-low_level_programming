490200fce279104420c6b23e42f3ffff336d53d6
Tomiwa Awogun <jawogun@gmail.com>
2022-09-13 09:20:59 -0700

    changes2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,10 +5,17 @@
  * @n: number inputed
  * Return: returns the last digit of the number
  */
-int print_last_digit(int n)
+int print_last_digit(int c)
 {
-	if (n < 0 || n == 0)
-	n = -n;
-	_putchar(n % 10 + '0');
-	return (n % 10);
+	if (c > 0 || c == 0)
+	{
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
+	else
+	{
+	c = c * -1;
+	_putchar (c % 10 + '0');
+	return (c % 10);
+	}
 }
