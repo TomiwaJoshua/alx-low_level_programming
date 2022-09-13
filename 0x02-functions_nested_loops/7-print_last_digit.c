b1da3591d27e1b9d55494d54f8b2082c34fb8cd3
Tomiwa Awogun <jawogun@gmail.com>
2022-09-13 10:51:18 -0700

    changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * print_last_digit - prints the last digit of a number
- * @c: number inputed
- * Return: returns the last digit of the number
+ * print_last_digit - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
+ * Return: 0
  */
 int print_last_digit(int c)
 {
