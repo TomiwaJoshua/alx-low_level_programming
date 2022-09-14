6217287ccd5ba79355988b255f257f68b2726523
Tomiwa Joshua <jawogun@gmail.com>
2022-09-14 22:27:52 +0000

    changes

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a, sum, n, p, lib;
+
+	n = 1;
+	p = 0;
+	lib = 0;
+
+	for (a = 1; a <= 50; a++)
+	{
+		sum = n + p;
+		if (((sum % 2) == 0) || (sum < 4000000))
+		{
+		lib = lib + sum;
+		}
+		p = n;
+		n = sum;
+	}
+	printf("%ld\n", lib);
+	return (0);
+}
