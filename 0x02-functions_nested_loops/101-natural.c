df2d23c082c248182b7fad07d5132066f8918c1e
Tomiwa Joshua <jawogun@gmail.com>
2022-09-14 21:20:05 +0000

    changes

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, sum;
+
+	sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (((a % 3) == 0) || ((a % 5) == 0))
+		{
+		sum = a + sum;
+		}
+	}
+	printf("%d\n", sum);
+}
