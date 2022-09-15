790efb91b1430cb10740043524eb570af46b8b3d
Tomiwa Awogun <jawogun@gmail.com>
2022-09-14 23:33:26 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all single digit numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char x[] = "01356789";
+	int r = 0;
+
+	while (r < 7)
+	{
+	putchar(x[r]);
+	r++;
+	}
+	_putchar('\n');
+	return (0);
+}
