312a4cc610d435181ce2e56678e47e2795d022ec
Tomiwa Joshua <jawogun@gmail.com>
2022-09-19 22:31:37 +0000

    changes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,12 @@ void print_array(int *a, int n)
 
 	if (n > 0)
 	{
-	printf("%d", a[0]);
+		printf("%d", a[0]);
 
-	for (i = 1; i < n; i++)
-	{
+		for (i = 1; i < n; i++)
+		{
 		printf(", %d", a[i]);
+		}
 	}
 	printf("\n");
-	}
 }
