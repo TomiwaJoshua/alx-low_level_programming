d0e2fc68338fdf2d6430f68f8365846c5cad5217
Tomiwa Joshua <jawogun@gmail.com>
2022-09-19 20:21:29 +0000

    changes

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates length of a string
+ * @str: string with length to be determined
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
