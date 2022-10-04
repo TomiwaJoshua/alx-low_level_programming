30b15c4c30f7f693862be0f68a5f54a53f2f2591
Tomiwa Joshua <jawogun@gmail.com>
2022-10-04 19:17:00 +0000

    changes

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -73,5 +73,7 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+int count_word(char *s);
+char **strtow(char *str);
 
 #endif
