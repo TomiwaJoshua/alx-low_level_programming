026898f0363e3d6c2118022088f91a1fba043527
Tomiwa Joshua <jawogun@gmail.com>
2022-10-11 01:53:31 +0000

    changes

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,13 +8,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d->name != NULL)
-		free(d->name);
-	if (d->age != NULL)
-		free(d->age);
-	if (d->owner != NULL)
-		free(d->owner);
 	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
 		free(d);
-	Return(NULL);
+	}
 }
