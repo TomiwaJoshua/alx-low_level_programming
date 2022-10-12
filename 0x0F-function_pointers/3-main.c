8f8e6cc4a447da643491e77584b33216c34a22f1
Tomiwa Joshua <jawogun@gmail.com>
2022-10-12 22:49:47 +0000

    changes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+
+/**
+ * main - program to perform simple operations
+ * @argc: numbeer of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char o;
+	int (*oper)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	oper = get_op_func(argv[2]);
+
+	if (!oper)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = oper(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result of the arithmetic operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+#include "3-calc.h"
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result of the arithmetic operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+#include "3-calc.h"
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result of the arithmetic operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+#include "3-calc.h"
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result of the arithmetic operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+#include "3-calc.h"
+
+/**
+ * op_mod - finds remainder of division of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the result of the arithmetic operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
