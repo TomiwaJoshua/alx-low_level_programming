ea778ef67f90ab3cf377eb5763605aecdffee6f9
Tomiwa Joshua <jawogun@gmail.com>
2022-10-25 18:19:36 +0000

    changes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: pointer to the head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = (listint_t *)malloc(sizeof(listint_t));
+	if (!temp)
+		return (NULL);
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
