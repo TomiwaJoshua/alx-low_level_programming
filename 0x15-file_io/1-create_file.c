9e76ed9e9b9da9fa8830f02abf3a82606fd4eb0d
Tomiwa Joshua <jawogun@gmail.com>
2022-10-31 18:39:53 +0000

    changes

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text to write into the file
+ * Return: returns 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t wwrite, oopen, len;
+
+	len = 0;
+
+	if (!text_content)
+		text_content = "";
+	if (!filename)
+		return (-1);
+	while (text_content)
+		len++;
+
+	oopen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (oopen == -1)
+		return (-1);
+
+	wwrite = write(oopen, text_content, len);
+	if (wwrite == -1)
+		return (-1);
+
+	return (1);
+}
