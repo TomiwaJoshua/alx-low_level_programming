bee7cbfb779abc2f4f239d9445c323b3dc449d78
Tomiwa Joshua <jawogun@gmail.com>
2022-10-31 18:01:16 +0000

    changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * a
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: text file input
+ * @letters: number of letters it should read and print
+ * Return: returns the actual number of letters it could read and print;
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t file, wwrite, actlet;
+	char *buff;
 
+	buff = malloc(letters);
+
+	if (!buff)
+		return (0);
+	if (!filename)
+		return (0);
+
+	file = open(filename, 0_RDONLY);
+
+	if (file == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	actlet = read(file, buff, letters);
+	wwrite = write(STDOUT_FILENO, buff, actlet);
+
+	if (wwrite == -1 || wwrite != letters)
+		return (0);
+
+	close(file)
+	return (wwrite);
 }
